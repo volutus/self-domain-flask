0ab47f7c60cf87b9db101857ff78956214a04bfc
volutus <trevcat3@hotmail.com>
2024-05-23 19:56:08 -0400

    Initial piece logic + piece emojis

--- app.py
+++ app.py
@@ -11,9 +11,35 @@ def home():
 
 @app.route("/chess")
 def chess():
+    # TODO this should be wrapped in a class which will contain the hex / bytes logic, map, and piece color
+    # the application should only expect a list of ChessPiece objects which would have all needed fields as public
+    starting_state = '846ac6482222222200000000000000000000000000000000111111117359b537'
+    pieces = list()
+    piece_map = dict()
+    piece_map['0'] = ('', 'w')
+    piece_map['1'] = ('♙', 'w')
+    piece_map['2'] = ('♟', 'b')
+    piece_map['3'] = ('♘', 'w')
+    piece_map['4'] = ('♞', 'b')
+    piece_map['5'] = ('♗', 'w')
+    piece_map['6'] = ('♝', 'b')
+    piece_map['7'] = ('♖', 'w')
+    piece_map['8'] = ('♜', 'b')
+    piece_map['9'] = ('♕', 'w')
+    piece_map['a'] = ('♛', 'b')
+    piece_map['b'] = ('♔', 'w')
+    piece_map['c'] = ('♚', 'b')
+    piece_map['d'] = ('?', 'w')
+    piece_map['e'] = ('?', 'w')
+    piece_map['f'] = ('?', 'w')
+
+    for e in starting_state:
+        pieces.append(piece_map[e])
+
     content = dict()
     content['rows'] = [8, 7, 6, 5, 4, 3, 2, 1]
     content['squares'] = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H']
+    content['pieces'] = pieces
     return render_template("chessboard.html", **content)
 
 
