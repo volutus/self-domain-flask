df17947e38798c8fb1bf9f13147cc7a20ddbfc5c
volutus <trevcat3@hotmail.com>
2024-06-01 22:44:08 -0400

    Rework move function, prep for server-side logic

--- static/js/chess.js
+++ static/js/chess.js
@@ -1,28 +1,56 @@
 let selectedPiece = undefined;
+let selectedSquare = undefined;
 let squares = document.querySelectorAll(".square");
 squares.forEach(function(square)
 {
     square.addEventListener("click", function()
     {
-        document.querySelectorAll(".selected").forEach(function(selected)
-        {
-            selected.classList.remove("selected");
-        });
-        this.classList.add("selected");
-
         const coordinate = this.getAttribute("id");
-        const piece = getPiece(coordinate);
-        if (selectedPiece != undefined)
+        // Handle selection logic for the same square
+        if (selectedSquare != undefined && coordinate == selectedSquare)
         {
-            move(selectedPiece, coordinate);
+            const highlighted = document.querySelectorAll(".selected").length > 0;
+            if (highlighted)
+            {
+                clearHighlight();
+                resetSelections();
+            }
+            else
+            {
+                highlight(this);
+            }
+            return;
         }
-        if (piece != undefined)
+
+        // Reset highlighter state
+        clearHighlight();
+        highlight(this);
+        selectedSquare = coordinate;
+
+        const piece = getPiece(coordinate);
+        if (selectedPiece != undefined && selectedPiece != coordinate)
+        {
+            move(selectedPiece, coordinate, this);
+            resetSelections();
+        }
+        else if (piece != undefined)
         {
             selectedPiece = coordinate;
         }
     });
 });
 
+function clearHighlight()
+{
+    document.querySelectorAll(".selected").forEach(function(selected)
+    {
+        selected.classList.remove("selected");
+    });
+}
+function highlight(element)
+{
+    element.classList.add("selected");
+}
 function move(source, destination)
 {
     const piece = getPiece(source);
@@ -31,11 +59,27 @@ function move(source, destination)
         return;
     }
 
+    // TODO Handle response!
+    response = attemptMove(source, destination);
+
+    // IF VALID
     document.getElementById(destination).innerHTML = piece.outerHTML;
     document.getElementById(source).innerHTML = '';
+    clearHighlight();
+}
+
+function resetSelections()
+{
+    selectedSquare = undefined;
+    selectedPiece = undefined;
 }
 function getPiece(coordinate)
 {
     const element = document.getElementById(coordinate);
     return element.querySelector(".piece");
+}
+function attemptMove(source, destination)
+{
+    // TODO tell the server our move and see what it says
+
 }
