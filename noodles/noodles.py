bf7fde6ae83ad65017211fce09f386dc462cd35c
volutus <trevcat3@hotmail.com>
2026-03-09 22:29:08 -0400

    try hardcoded DB name value for container

--- noodles/noodles.py
+++ noodles/noodles.py
@@ -3,7 +3,8 @@ import psycopg2
 
 def fetch_dict():
     # init DB (pull me into a utility if this comes up often)
-    db_host = os.environ['DB_HOST']
+    # db_host = os.environ['DB_HOST']
+    db_host = 'self-domain-docker-db-1'
     db_user = os.environ['DB_USERNAME']
     db_pass = os.environ['DB_PASSWORD']
     conn = psycopg2.connect(host=db_host, database="postgres", user=db_user, password=db_pass)
