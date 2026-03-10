32021f038873304a8666b493635e94838cee6e04
volutus <trevcat3@hotmail.com>
2026-03-09 23:24:39 -0400

    env secrets folder + HTML template

--- noodles/noodles.py
+++ noodles/noodles.py
@@ -3,8 +3,7 @@ import psycopg2
 
 def fetch_reviews():
     # init DB (pull me into a utility if this comes up often)
-    # db_host = os.environ['DB_HOST']
-    db_host = 'self-domain-docker-db-1'
+    db_host = os.environ['DB_HOST']
     db_user = os.environ['DB_USERNAME']
     db_pass = os.environ['DB_PASSWORD']
     conn = psycopg2.connect(host=db_host, database="postgres", user=db_user, password=db_pass)
@@ -18,10 +17,14 @@ def fetch_reviews():
     
     cur = conn.cursor()
     cur.execute(sql)
-    reviews = cur.fetchall()
+    columns = [column[0] for column in cur.description]
+    reviews = []
+    for row in cur.fetchall():
+        reviews.append(dict(zip(columns, row)))
+
     cur.close()
     conn.close()
-    
+
     return reviews
     
     
