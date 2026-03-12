a132f1d879b07fe5584dc9a1899f7409076b4303
volutus <trevcat3@hotmail.com>
2026-03-12 18:32:09 -0400

    Update query to sort by review_date DESC

--- noodles/noodles.py
+++ noodles/noodles.py
@@ -14,6 +14,7 @@ def fetch_reviews():
     from noodle_review nr
     join noodles no on nr.noodle_id = no.id 
     join noodle_maker nm on no.maker_id = nm.id
+    order by review_date desc;
     '''
     
     cur = conn.cursor()
